b167b85a27b5dbcd6fc6e8f77c7d8f4e6bd89cb7
Axel Guinot <axel.guinot.astro@gmail.com>
2024-06-24 04:01:48 +0200

    add multi_obj script

---
+++ script/shape_multi_runner.py
@@ -0,0 +1,69 @@
+from argparse import ArgumentParser
+import os
+import logging
+from datetime import datetime
+
+from LenSimu.postprocessing.postprocess import PostProcessDetect
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument(
+        "stamp_index",
+        help="Tile index to simulate from the tile IDs catalogue.",
+        type=int,
+    )
+    parser.add_argument(
+        "-i",
+        "--input_dir",
+        dest="input_dir",
+        help="path to the input directory (output of stamp_runner)",
+        type=str,
+    )
+    parser.add_argument(
+        "-v",
+        "--vignet_size",
+        dest="vignet_size",
+        help="size of the vignet for the shape measurement (default: 51)",
+        default=51,
+        type=int,
+    )
+
+    args = parser.parse_args()
+
+    start = args.stamp_index * 1_000
+    stop = (args.stamp_index + 1) * 1_000
+
+    logging.basicConfig(
+        filename=f"/home/guinot/n17data/simu_LenSimu/logs/log-shape_m-{args.stamp_index}.txt",  # noqa
+        level=logging.INFO,
+    )
+    logger = logging.getLogger("stamp_runner")
+
+    for i in range(start, stop):
+        logger.info("#####")
+        logger.info("time: " + datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+        logger.info(i)
+        # if os.path.exists(
+        #     f"/n17data/guinot/simu_LenSimu/output_stamp/{i}/shear_0.00_-0.02/final_cat.npy"  # noqa
+        # ):
+        #     logger.info("skipping")
+        #     continue
+
+        try:
+            pp = PostProcessDetect(
+                i,
+                args.input_dir,
+                args.vignet_size,
+            )
+
+            pp.go()
+        except Exception:
+            logger.info("failed", i)
+            continue
+        if os.path.exists(
+            f"/n17data/guinot/simu_LenSimu/output_stamp/{i}/shear_0.00_-0.02/final_cat.npy"  # noqa
+        ):
+            logger.info("done")
+        else:
+            logger.info("failed, should not appear!!")
