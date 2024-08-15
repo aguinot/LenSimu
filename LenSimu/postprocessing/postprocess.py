c1eff303366e78f7bd52c6566e798bb22573c0e9
aguinot <axel.guinot.astro@gmail.com>
2024-08-14 23:06:15 -0400

    Clean up postprocessing

--- LenSimu/postprocessing/postprocess.py
+++ LenSimu/postprocessing/postprocess.py
@@ -206,70 +206,8 @@ class PostProcessDetect(PostProcess):
             exp_nums.append([int(exp_num), int(ccd_id)])
         self.exp_nums = np.array(exp_nums)
 
-    def get_shape(self, dir_path):
-        coadd_img = fits.open(os.path.join(dir_path, self.coadd_img_name))
-
-        try:
-            name = coadd_img[3].name
-            if name == "MASK":
-                mask = coadd_img[3].data
-        except Exception:
-            mask = np.zeros_like(coadd_img[1].data)
-
-        cat, seg = get_cat(
-            coadd_img[1].data,
-            coadd_img[2].data,
-            mask,
-            coadd_img[1].header,
-            1.5,
-        )
-
-        if sum(cat["central_flag"]) == 0:
-            return ["fail"], None
-        else:
-            ind_central = np.where(cat["central_flag"] == 1)[0]
-
-        coadd_mask = self._get_seg_mask(
-            cat["x"][ind_central][0],
-            cat["y"][ind_central][0],
-            seg,
-            cat["number"][ind_central],
-        )
-
-        img_vign, jacob_list = self._get_img_info(
-            cat["ra"][ind_central][0],
-            cat["dec"][ind_central][0],
-            dir_path,
-            coadd_mask,
-        )
-        gals = img_vign["SCI"]
-        weights = img_vign["WEIGHT"]
-        flags = img_vign["MASK"]
-        psfs, psfs_sigma = self._get_psf_info(dir_path)
-        prior = get_prior()
-
-        try:
-            res = do_ngmix_metacal(
-                gals,
-                psfs,
-                psfs_sigma,
-                weights,
-                flags,
-                jacob_list,
-                prior,
-            )
-            res["obj_id"] = self.stamp_id
-            res["n_epoch_model"] = len(gals)
-            print(res["noshear"]["g"])
-        except Exception:
-            res = "fail"
-
-        return [res], cat[ind_central]
-
     def _get_seg_mask(self, x, y, seg, seg_id):
-        fs = FetchStamps(seg, int(self.vign_size / 2))
-        fs.get_pixels(np.round([[x, y]]).astype(int))
-        vign_ = fs.scan()[0].astype(np.float64)
+        vign_ = get_stamp(seg, x, y, self.vign_size)
 
         vign_[vign_ == seg_id] = 0
         vign_[vign_ > 0] = 1
@@ -286,13 +224,41 @@ class PostProcessDetect(PostProcess):
     def go(self):
         all_outputs = []
         for shear_dir in self.shear_dirs:
-            res, coadd_cat = self.get_shape(shear_dir)
+            coadd_img = fits.open(os.path.join(shear_dir, self.coadd_img_name))
+            if coadd_img[3].name == "MASK":
+                mask = coadd_img[3].data
+            cat, seg = get_cat(
+                coadd_img[1].data,
+                coadd_img[2].data,
+                mask,
+                coadd_img[1].header,
+                1.5,
+            )
+            if sum(cat["central_flag"]) == 0:
+                continue
+            else:
+                ind_central = np.where(cat["central_flag"] == 1)[0]
+            coadd_mask = self._get_seg_mask(
+                cat["x"][ind_central][0],
+                cat["y"][ind_central][0],
+                seg,
+                cat["number"][ind_central],
+            )
+            img_vign, jacob_list = self._get_img_info(
+                cat["ra"][ind_central][0],
+                cat["dec"][ind_central][0],
+                shear_dir,
+                coadd_mask,
+            )
+            psfs, psfs_sigma = self._get_psf_info(shear_dir)
+
+            res = self.get_shape(img_vign, jacob_list, psfs, psfs_sigma)
             if res[0] == "fail":
                 continue
 
             final_res = compile_results(res)
             final_res.update(
-                {key: coadd_cat[key] for key in coadd_cat.dtype.names}
+                {key: cat[ind_central][key] for key in cat.dtype.names}
             )
             all_outputs.append((shear_dir, final_res))
 
